a9983dc2785bc81f1222d0116509a39fd272d52c
lilith18 <41243398+lilith18@users.noreply.github.com>
2018-08-05 11:28:54 +0100

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.picadilly.picadilly;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
